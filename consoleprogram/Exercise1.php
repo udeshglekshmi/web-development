aa78be75a583f67a285f189fb5ee90b9b24df4d0
Udeshkumar <udesh4dev@gmail.com>
2024-07-08 07:36:35 +0530

    Add Exercise programs

---
+++ consoleprogram/Exercise1.php
@@ -0,0 +1,6 @@
+<?php
+$text="Hello PHP"; 
+$n1 = 459; 
+$n2 = 345; 
+$n3 = $n2 + $n2 + $n1;  
+echo $text." you have added ".$n3; 
---
+++ consoleprogram/Exercise2.php
@@ -0,0 +1,6 @@
+<?php
+$name = readLine("Type some content and decimal value: "); 
+$number; 
+fscanf(STDIN,"%f",$number); 
+echo $number."\n"; 
+echo $name."\nthank\tyou!";   
